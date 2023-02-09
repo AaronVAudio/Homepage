c3747e94ff99b1e3c2497532c79fa4bf534116e5
AaronVAudio <46612724+AaronVAudio@users.noreply.github.com>
2023-02-08 20:53:34 -0500

    Add files via upload

--- Homepage/tc/y.js
+++ Homepage/tc/y.js
@@ -1,9 +1,14 @@
-function myClick() {
+function myClick(button) {
     var x = document.getElementById("thing");
-    if (x.style.color === "yellow" || x.style.color === "#ffff00") {
-        x.style.color = "brown";
-    } else {
-        x.style.color = "yellow";
+    if (button === "butt") {
+        if (x.style.color === "yellow" || x.style.color === "#ffff00") {
+            x.style.color = "brown";
+        } else {
+            x.style.color = "yellow";
+        }
     }
-    document.getElementById("go").style.display = "block";
+    else if (button === "thing") {
+        document.getElementById("go").style.display = "block";
+        document.getElementById("thing").style.marginTop = "100%";
+    }
 }
--- Homepage/tc/z.js
+++ Homepage/tc/z.js
@@ -1,7 +1,7 @@
 function myClick() {
     var name=document.getElementById("name").value;
     if (name.toLowerCase()==="alicia") {
-        document.getElementById("output").innerHTML = "Oh, it's you.";
+        document.getElementById("output").innerHTML = "Ugh, you again.";
         document.getElementById("output2").innerHTML = "";
         document.body.style.backgroundColour = "black";
     } else if (name==="") {
@@ -14,14 +14,14 @@ function myClick() {
 }
 function myClick2() {
     var name=document.getElementById("name").value;
-    if (document.getElementById("output").innerHTML === "THE" || document.getElementById("output").innerHTML === "Good.") {
-    } else if (document.getElementById("output").innerHTML === "Stop it.") {
-        if (confirm("Seriously.")) {
-            document.getElementById("output").innerHTML = "Good.";
+    if (document.getElementById("output").innerHTML === "THE" || document.getElementById("output").innerHTML === "") {
+    } else if (document.getElementById("output").innerHTML === "You know how this goes.") {
+        if (confirm("You're ok I guess.")) {
+            document.getElementById("output").innerHTML = "";
             document.getElementById("output2").innerHTML = "";
             open("y.html", "_self");
         } else {
-            document.getElementById("output").innerHTML = ">:(";
+            document.getElementById("output").innerHTML = ":P";
             document.getElementById("output2").innerHTML = "";
         }
     } else if (document.getElementById("output2").innerHTML === "You found everything.") {
@@ -31,8 +31,8 @@ function myClick2() {
         } else {
             open("y.html", "_self");
         }
-    } else if (name.toLowerCase() === "alicia" && document.getElementById("output").innerHTML === "Oh, it's you.") {
-        document.getElementById("output2").innerHTML = "I should have known.";
+    } else if (name.toLowerCase() === "alicia" && document.getElementById("output").innerHTML === "Ugh, you again.") {
+        document.getElementById("output2").innerHTML = "You came back.";
     } else if (name==="") {
     } else if (name.toLowerCase() !== "alicia") {
         document.getElementById("output2").innerHTML = "I've been expecting you.";
@@ -40,9 +40,9 @@ function myClick2() {
 }
 function myClick3() {
     var name=document.getElementById("name").value;
-    if (document.getElementById("output").innerHTML === "THE" || document.getElementById("output").innerHTML === "Good.") {
+    if (document.getElementById("output").innerHTML === "THE" || document.getElementById("output").innerHTML === "") {
     } else if (name.toLowerCase()==="alicia") {
-        document.getElementById("output").innerHTML = "Stop it.";
+        document.getElementById("output").innerHTML = "You know how this goes.";
         document.getElementById("output2").innerHTML = "";
     } else if (name==="") {
     } else {
