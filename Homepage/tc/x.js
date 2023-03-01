2ffe9df7b084764a02d7392bac17e9e8130b336f
AaronVAudio <46612724+AaronVAudio@users.noreply.github.com>
2023-02-28 21:19:39 -0500

    Add files via upload

--- Homepage/tc/x.js
+++ Homepage/tc/x.js
@@ -52,6 +52,7 @@ function onClick(button) {
             document.getElementById("content").innerHTML = "You take some popcorn, then continue on your way.<br>You enter the elevator room, press the button, and wait for the elevator.<br>You take the elevator down.<br>You hear something large moving outside the door.<br>The door opens, and you slowly step outside.<br>You hear a roar.<br>You hear a deep voice...<br><br>\""+user+"...\"<br><br>\""+user+"...\"<br><br><span class='bolded'You have awakened The Creature.</span>";
             document.getElementById("b1").innerHTML = "Run";
             document.getElementById("b2").innerHTML = "Throw The Popcorn";
+            document.getElementById("overlay").style.opacity = "50%";
             page = "4a";
         } else if (button === "b2") {
             document.getElementById("content").innerHTML = "You enter the elevator room.";
@@ -64,12 +65,16 @@ function onClick(button) {
             document.getElementById("content").innerHTML = "You try to run, but you hear something crashing behind you.<br>You feel something heavy hit you, and everything goes dark.<br><br><span class='bolded'>You have been eaten by The Creature.</span>";
             document.getElementById("b1").innerHTML = "Start Over";
             document.getElementById("b2").style.display = "none";
+            document.getElementById("overlay").style.display = "none";
+            document.getElementById("overlay").style.opacity = "0%";
             document.body.style.background = "red";
             page = "end";
         } else if (button === "b2") {
             document.getElementById("content").innerHTML = "You throw the popcorn into the darkness.<br>You hear another roar, and something huge crashing away from you.<br>You quickly dash to the garbage room and dump the boxes and bags.<br>You run back to the elevator, and hurriedly mash the buttons.<br>You hear a distant roar, and the crashing sound approaches as the doors begin to close.<br>You see a huge shadow at the last second as the door closes and the elevator begins to rise.<br><br><span class='bolded'>You have evaded The Creature.<br>You have successfully taken down the garbage.<br>Congratulations!</span>";
             document.getElementById("b1").innerHTML = "THE END";
             document.getElementById("b2").style.display = "none";
+            document.getElementById("overlay").style.display = "none";
+            document.getElementById("overlay").style.opacity = "0%";
             document.body.style.background = "gold";
             page = "win";
         }
@@ -79,6 +84,7 @@ function onClick(button) {
             document.getElementById("b1").innerHTML = "Try To Lift It";
             document.getElementById("b2").innerHTML = "Use The Cart";
             document.getElementById("b2").style.display = "inline";
+            document.getElementById("overlay").style.display = "block";
             page = "1";
             document.body.style.background = "lightgreen";
         }
